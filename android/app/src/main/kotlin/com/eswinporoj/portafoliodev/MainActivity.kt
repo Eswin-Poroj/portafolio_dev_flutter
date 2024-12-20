bcfee72b55862f10e18100941296b31b14536137
Eswin Poroj <eporojc1@miumg.edu.gt>
2024-12-19 20:33:53 -0600

    first commit

---
+++ android/app/src/main/kotlin/com/eswinporoj/portafoliodev/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.eswinporoj.portafoliodev
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PortfolioApp());
+}
+
+class PortfolioApp extends StatelessWidget {
+  const PortfolioApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Mi Portafolio',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mi Portafolio'),
+      ),
+      body: const Text('hola mundo'),
+    );
+  }
+}
